2a7af8a27abb0ad15700ff9cbabffd556af768ae
David Scholten <134960429+dave-sc@users.noreply.github.com>
2020-08-16 20:16:38 +0200

    Add status test controller

---
+++ LoRaWeatherStation.Service/Controllers/StatusController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LoRaWeatherStation.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : ControllerBase
+    {
+        private readonly ILogger<StatusController> _logger;
+
+        public StatusController(ILogger<StatusController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        public string Get()
+        {
+            return "healthy";
+        }
+    }
+}
