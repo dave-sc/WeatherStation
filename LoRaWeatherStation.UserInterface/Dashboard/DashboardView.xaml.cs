502a97d8a3d501055e7e34e71f46eb0b42ecbfa9
David Scholten <134960429+dave-sc@users.noreply.github.com>
2020-08-23 11:48:03 +0200

    Add clock widget to dashboard

--- LoRaWeatherStation.UserInterface/Dashboard/DashboardView.xaml.cs
+++ LoRaWeatherStation.UserInterface/Dashboard/DashboardView.xaml.cs
@@ -1,13 +1,24 @@
+using System.Reactive.Disposables;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
+using LoRaWeatherStation.UserInterface.Controls;
+using ReactiveUI;
 
 namespace LoRaWeatherStation.UserInterface.Dashboard
 {
     public class DashboardView : ReactiveUserControl<DashboardViewModel>
     {
+        private ClockControl ClockControl => this.FindControl<ClockControl>("Clock");
+        
         public DashboardView()
         {
             InitializeComponent();
+            this.WhenActivated(disposables =>
+            {
+                this.Bind(ViewModel, vm => vm.CurrentTime, v => v.ClockControl.Time)
+                    .DisposeWith(disposables);
+            });
         }
 
         private void InitializeComponent()
