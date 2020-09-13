56b1c7e66c4c5a1c98844ccbc925806ed60a2efc
David Scholten <134960429+dave-sc@users.noreply.github.com>
2020-09-13 14:28:01 +0200

    Add an extension method to enumerate all values of a tuple

---
+++ LoRaWeatherStation.Utils/TupleExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace LoRaWeatherStation.Utils
+{
+    public static class TupleExtensions
+    {
+        public static IEnumerable AsEnumerable(this ITuple tuple)
+        {
+            for (int i = 0; i < tuple.Length; i++)
+                yield return tuple[i];
+        }
+    }
+}
