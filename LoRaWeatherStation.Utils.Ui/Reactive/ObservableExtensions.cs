bc41d05c89d461104218be58b920467f0c17a0b3
David Scholten <134960429+dave-sc@users.noreply.github.com>
2020-09-13 14:25:34 +0200

    Add reactive extension method to do Select-operations with asynchronous methods

--- LoRaWeatherStation.Utils.Ui/Reactive/ObservableExtensions.cs
+++ LoRaWeatherStation.Utils.Ui/Reactive/ObservableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 
 namespace LoRaWeatherStation.Utils.Reactive
 {
@@ -21,5 +22,17 @@ namespace LoRaWeatherStation.Utils.Reactive
             => source.Select(t => selector(t.Item1, t.Item2, t.Item3, t.Item4, t.Item5, t.Item6, t.Item7, t.Item8));
         public static IObservable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(this IObservable<(T1, T2, T3, T4, T5, T6, T7, T8, T9)> source, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> selector)
             => source.Select(t => selector(t.Item1, t.Item2, t.Item3, t.Item4, t.Item5, t.Item6, t.Item7, t.Item8, t.Item9));
+        
+        public static IObservable<TResult> Select<TObservable, TResult>(this IObservable<TObservable> observable, Func<Task<TResult>> selector)
+        {
+            return observable.Select(_ => Observable.FromAsync(selector))
+                .Concat();
+        }
+        
+        public static IObservable<TResult> Select<TObservable, TResult>(this IObservable<TObservable> observable, Func<TObservable, Task<TResult>> selector)
+        {
+            return observable.Select(x => Observable.FromAsync(() => selector(x)))
+                .Concat();
+        }
     }
 }
